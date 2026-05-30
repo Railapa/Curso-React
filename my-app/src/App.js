b21c912ffbfb93845d6243a7ae734edfeb637424
Railapa <rai100304@gmail.com>
2026-05-30 12:54:54 -0300

    Curso React

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,7 @@
 import './App.css';
-import HellWorld from './components/HelloWorld';
 
 function App() {
-  const name = 'Matheus'
-  const nameUpper = name.toUpperCase()
-
-  const soma = (a,b) => {
-    return a + b
-  }
-
-  const url = 'https://via.placeholder.com/150' // Não está funcionando o url da aula
-
-  return (
-    <div className="App">
-      <h2>Alterando o JSX</h2>
-      <p>Olá, {nameUpper}</p>
-      <p>Soma: {soma(1,2)}</p>
-      <img scr={url} alt="Minha imagem"/>
-      <HellWorld></HellWorld>
-    </div>
-  );
+  
 }
 
 export default App;
