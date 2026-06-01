bfa12e7fbaf58c594fc92f417499117c08ba9249
Railapa <rai100304@gmail.com>
2026-06-01 12:45:15 -0300

    Curso React

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,21 @@
 import './App.css';
+//import HelloWorld from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 
 function App() {
-  
+
+  const nome = 'Maria'
+
+  return (
+    <div className='App'>
+
+      <SayMyName nome='Matheus'></SayMyName>
+      <SayMyName nome={nome}></SayMyName>
+
+      <Pessoa nome='Bruno' idade='32' profissao='Programador' foto='https://via.placeholder/150'></Pessoa>
+    </div>
+  )
 }
 
 export default App;
